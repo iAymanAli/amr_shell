a4bc07fa897e2d91d2cde65f58d02827eca8860e
Ayman H Ali <aymanhashem0001@gmail.com>
2026-08-29 16:01:50 +0300

    fixed comments in include/globals.h

--- include/globals.h
+++ include/globals.h
@@ -3,7 +3,7 @@
 
 #include <stdbool.h>
 
-// A struct that stores information for the complete builtin command
+// Struct that stores information for the complete built-in command
 typedef struct {
     bool populated;
     char cmd[255];
@@ -12,19 +12,19 @@ typedef struct {
     char arg_cmd[255];
 }comp;
 
-// Determine the path delimiter and path separator for Unix based systems
+// PATH delimiter and separator for Unix based systems.
 #define PATH_DELIMITER ":"
 #define PATH_SEPARATOR "/"
 
-#define CUSTOM_MAX_ARGS 128  // Custom maximum number of command arguments
-#define KB 1024  // Size of kilobyte for some strings and arrays
+#define CUSTOM_MAX_ARGS 128  // Custom maximum number of command arguments.
+#define KB 1024  // Size of kilobyte for some strings and arrays.
 
-extern const char *builtins[];  // Global array storing built-in commands names
-extern char *commands_arr[4 * KB];  // Global array storing built-in commands names and programs names from PATH
+extern const char *builtins[];  // Global array storing built-in commands names.
+extern char *commands_arr[4 * KB];  // Global array storing built-in commands names and commands names from PATH.
 
-extern comp comp_arr[KB];  // Global array storing structs for commands that has been registered with the complete built-in command 
+extern comp comp_arr[KB];  // Global array storing structs for commands that has been registered with the complete built-in command.
 
-// Global array that stores possible matches for a command arguments that was registered with complete built-in command
+// Global array that stores possible matches for a command arguments that was registered with complete built-in command.
 extern char *comp_words[KB];  
 
 // Necessary indexes and flags for piping feature
